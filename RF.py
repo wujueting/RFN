0ea3dae203ffb5a2650784ff0852af4b4bde8426
wujueting <403457198@qq.com>
2024-11-27 09:40:01 +0800

    Update RF.py

--- RF.py
+++ RF.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    lgbm = joblib.load('./model.pkl')   # 本地测试路径，根据pkl文件所在位置进行更改
-    # lgbm = joblib.load('./lgbm.pkl')  # 上传到github所需路径，路径无需更改
+    lgbm = joblib.load('./model.pkl')
 
     class Subject:
         def __init__(self, Age,Audiogram_type,ALB,Degree_of_hearing_loss,MCV,Onset_to_treatment):
-            self.age = Age
+            self.Age = Age
             self.Audiogram_type = Audiogram_type
             self.ALB = ALB
             self.Degree_of_hearing_loss = Degree_of_hearing_loss
@@ -22,7 +21,7 @@ def main():
 
         def make_predict(self):
             subject_data = {
-                "Age": [self.age],
+                "Age": [self.Age],
                 "Audiogram_type": [self.Audiogram_type],
                 "ALB": [self.ALB],
                 "Degree_of_hearing_loss": [self.Degree_of_hearing_loss],
@@ -46,11 +45,7 @@ def main():
 
             explainer = shap.Explainer(lgbm)
             shap_values = explainer.shap_values(df_subject)
-            # 力图
             shap.force_plot(explainer.expected_value[1], shap_values[1][0, :], df_subject.iloc[0, :], matplotlib=True)
-            # 瀑布图
-            # ex = shap.Explanation(shap_values[1][0, :], explainer.expected_value[1], df_subject.iloc[0, :])
-            # shap.waterfall_plot(ex)
             st.pyplot(plt.gcf())
 
     st.set_page_config(page_title='Hearing recovery of sudden sensorineural hearing loss')
@@ -61,7 +56,7 @@ def main():
                 </div>
                 """, unsafe_allow_html=True)
     Age = st.number_input("Age (years)", value=50.5)
-    Audiogram_type = st.selectbox("Audiogram_type (Ascending = 0, flat = 1, descending= 2, profound= 3 )", [0, 1, 2 , 3 ], index=0)
+    Audiogram_type = st.selectbox("Audiogram type (Ascending = 0, flat = 1, descending= 2, profound= 3 )", [0, 1, 2 , 3 ], index=0)
     ALB = st.number_input("ALB (g/dl)", value=44.75)
     Degree_of_hearing_loss = st.selectbox("Degree_of_hearing_loss (Normal = 0, Mild = 1, Moderate= 2, Moderately severe= 3 ,Severe=4,  Profound=5, Complete=6 )", [0, 1, 2 , 3 ,4 , 5, 6 ], index=0)
     MCV = st.number_input("MCV (fl)", value=88.85)
